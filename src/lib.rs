46bd63808ff016c5aa774fdec576ef2bc3b95d0a
Константин <zuker_gta@mail.ru>
2025-06-27 20:56:46 +1000

    adder2

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn exploration() {
         assert_eq!(2 + 2, 4);
+    }
+    /*#[test]
+    fn another() {
+        panic!("Сделать этот тест неуспешным");
+    }*/
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle { length: 8, width: 7 };
+        let smaller = Rectangle { length: 5, width: 1 };
+        assert!(larger.can_hold(&smaller));
+    }
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle { length: 8, width: 7 };
+        let smaller = Rectangle { length: 5, width: 1 };
+        assert!(!smaller.can_hold(&larger));
+    }
+}
+
+#[derive(Debug)]
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
     }
 }
